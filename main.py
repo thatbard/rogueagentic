196ff018a75f46c2a5172495c2a95030c2fb17f6
thatbard <austin.mathews@gmail.com>
2025-12-24 09:27:43 -0700

    Accept user prompt and verbose output flag

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
 model = "gemini-2.5-flash"
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
     load_dotenv()
@@ -12,15 +19,17 @@ def main():
         raise RuntimeError("failed to load GEMINI_API_KEY")
     
     client = genai.Client(api_key=api_key)
-    request = "How much wood would a wood-chuck chuck if a wood-chuck could chuck wood?"
-    response = client.models.generate_content(model=model, contents=request)
+    request = args.user_prompt
+    response = client.models.generate_content(model=model, contents=messages)
 
     if response.usage_metadata == None:
         raise RuntimeError("no usage_metadata found")
     
-    print(f"User request: {request}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {request}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(f"Model response: {response.text}")
 
 
