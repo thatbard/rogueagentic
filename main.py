28d0ace39e808ce7243df7846698606ed39e6795
thatbard <austin.mathews@gmail.com>
2025-12-23 22:31:59 -0700

    Initial agent build test

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+model = "gemini-2.5-flash"
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("failed to load GEMINI_API_KEY")
+    
+    client = genai.Client(api_key=api_key)
+    request = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model=model, contents=request)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("no usage_metadata found")
+    
+    print(f"User request: {request}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Model response: {response.text}")
+
+
+if __name__ == "__main__":
+    main()
